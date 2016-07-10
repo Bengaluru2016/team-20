a6c1da968ff2465d7605526cf2940343e167ae73
Medha Bharadwaj <medhavkrishna@gmail.com>
2016-07-11 00:49:45 +0530

    new

--- android app1/app/src/main/java/com/example/medhab/ma/MainActivity.java
+++ android app1/app/src/main/java/com/example/medhab/ma/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.medhab.ma;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -17,6 +25,9 @@ public class MainActivity extends ActionBarActivity {
 
     EditText dob;
     Calendar myCalendar = Calendar.getInstance();
+    private static final int REQUEST_CAMERA = 1;
+    private static final int SELECT_FILE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +46,66 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        Button b=(Button)findViewById(R.id.SelectPhoto);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+
+            }
+        });
+
+
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo","Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                }  else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            ImageView Im=(ImageView)findViewById(R.id.Image);
+            if (requestCode == REQUEST_CAMERA) {
+
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                Im.setImageBitmap(thumbnail);
+                Log.d("done","done");
+            }
+            else
+            {
+                Log.d("undone","undone");
+            }
+        }
+        else
+        {
+            Log.d("undone","undone");
+        }
+
 
 
     }
 
 
 
-    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear,
