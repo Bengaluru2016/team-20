2d20264f504252aa04a90644be59947396ac450b
Medha Bharadwaj <medhavkrishna@gmail.com>
2016-07-10 21:43:18 +0530

    new app

--- webapp/samridhdhi/students/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from students.models import Student
-
-class studentAdmin(admin.ModelAdmin):
-	# list_display = ['name', 'gender', 'age', 'language', 'location', 'occupation', 'db', 'contactNumber', 'idProof', 'parentIncome']
-	class Meta:
-		model = Student
-admin.site.register(Student, studentAdmin)
